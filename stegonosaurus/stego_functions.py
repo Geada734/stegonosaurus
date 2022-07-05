bf7e7fddf05c008fd93fb4c8c511d1a35c226550
Geada734 <frankgeada@gmail.com>
2022-07-05 16:52:44 -0500

    Expanded the functionality for the inspect function

--- stegonosaurus/stego_functions.py
+++ stegonosaurus/stego_functions.py
@@ -2,11 +2,21 @@
 from PIL import Image
 from . import stego_utils as su
 
-def inspect(img: Image):
-    '''Inspects an image.'''
+def inspect(img: Image) -> str:
+    '''Returns each individual pixel and additional information
+    on the image input.'''
+    info_string = ""
     generator = su.image_reader(img)
 
     for i in generator:
-        print(i)
+        info_string + i + " "
 
-    img.show()
+    info_string += "\n" + "Filename: "
+
+    if hasattr(img, "filename"):
+        info_string += img.filename
+
+    info_string = info_string + "\nFormat: " + img.format + "\nMode:" + img.format + "\nSize: " +
+    img.width + "x" + img.height + "\n"
+
+    return info_string
