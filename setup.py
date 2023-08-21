8522bc40772dfae99782c346e63792951f6cc245
Geada734 <frankgeada@gmail.com>
2023-08-20 18:16:23 -0600

    Modified setup file to correct version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 setup(
     name='stegonosaurus',
     packages=find_packages(include=["stegonosaurus"]),
-    version='1.2.2',
+    version='1.2.3',
     description='Stegonography utilities',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
